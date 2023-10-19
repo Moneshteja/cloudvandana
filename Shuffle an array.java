92eeb1f92bce5757f78e7e2f8b87b6a7b4244531
Moneshteja <95086759+Moneshteja@users.noreply.github.com>
2023-10-19 20:43:30 +0530

    Update Shuffle an array.java

--- Shuffle an array.java
+++ Shuffle an array.java
@@ -1,18 +1,31 @@
-public class ShuffleArray {
+import java.util.Scanner;
+
+public class ArrayShuffle {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7};
-        shuffleArray(array);
-        for (int i : array) {
-            System.out.print(i + " ");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter numbers separated by spaces:");
+        String[] inputValues = scanner.nextLine().split(" ");
+        
+        int[] array = new int[inputValues.length];
+        for (int i = 0; i < inputValues.length; i++) {
+            array[i] = Integer.parseInt(inputValues[i]);
+        }
+
+        shuffle(array);
+        
+        System.out.println("Shuffled array:");
+        for (int num : array) {
+            System.out.print(num + " ");
         }
     }
 
-    public static void shuffleArray(int[] arr) {
-        for (int i = arr.length - 1; i > 0; i--) {
-            int randomIndex = (int)(Math.random() * (i + 1));
-            int temp = arr[i];
-            arr[i] = arr[randomIndex];
-            arr[randomIndex] = temp;
+    public static void shuffle(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int randomIndex = (int) (Math.random() * array.length);
+            int temp = array[i];
+            array[i] = array[randomIndex];
+            array[randomIndex] = temp;
         }
     }
 }
